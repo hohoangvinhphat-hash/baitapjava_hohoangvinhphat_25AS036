de992a8703be09f94df4296f8ce2a5c1dcd74411
hohoangvinhphat <hohoangvinhphat@gmail.com>
2025-11-07 08:17:27 +0700

    baitap1.5

--- src/baitap14/TestMain.java
+++ src/baitap14/TestMain.java
@@ -1,4 +1,24 @@
 package baitap14;
 
 public class TestMain {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee(27, "vinh", "phat", 3000);
+
+
+        System.out.println(e1);
+
+
+        System.out.println("Name: " + e1.getName());
+
+
+        System.out.println("Monthly Salary: " + e1.getSalary());
+        System.out.println("Annual Salary: " + e1.getAnnualSalary());
+
+
+        int newSalary = e1.raiseSalary(10);
+        System.out.println("After 10% raise, new salary: " + newSalary);
+
+        System.out.println(e1);
+    }
 }
