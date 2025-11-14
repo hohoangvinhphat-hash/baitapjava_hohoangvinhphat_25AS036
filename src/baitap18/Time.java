b215a24aff44025fb39b3883168611d94fb81f8e
hohoangvinhphat <hohoangvinhphat@gmail.com>
2025-11-14 08:12:52 +0700

    commit ex 1.8

---
+++ src/baitap18/Time.java
@@ -0,0 +1,86 @@
+package baitap18;
+
+public class Time {
+    // Thuộc tính
+    private int hour;
+    private int minute;
+    private int second;
+
+    // Constructor
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    // Getter
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    // Setter
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    // toString: định dạng hh:mm:ss với số 0 phía trước
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    // Tăng 1 giây
+    public Time nextSecond() {
+        second++;
+        if (second >= 60) {
+            second = 0;
+            minute++;
+            if (minute >= 60) {
+                minute = 0;
+                hour++;
+                if (hour >= 24) {
+                    hour = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    // Giảm 1 giây
+    public Time previousSecond() {
+        second--;
+        if (second < 0) {
+            second = 59;
+            minute--;
+            if (minute < 0) {
+                minute = 59;
+                hour--;
+                if (hour < 0) {
+                    hour = 23;
+                }
+            }
+        }
+        return this;
+    }
+}
